0999a8f243d33ae2cdac0352f574004e05606390
Toshiki Isawa <jack_the_fool@outlook.com>
2020-07-15 16:49:11 +0900

    slideDown & slideUp

--- app.js
+++ app.js
@@ -2,6 +2,25 @@
 //   $('.box1').slideDown();
 // });
 
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide();
+// });
+
 $(function(){
-  $('.box1').slideUp();
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
